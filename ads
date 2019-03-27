5612f790af9357d51304e483c7211c750c2de9b8
Katene Jack Cotton <katenejack@gmail.com>
2019-03-27 21:00:25 +1300

    compentized

---
+++ ads
@@ -0,0 +1,2 @@
+yarn add --save-dev webpack webpack-cli @babel/preset-react babel-loader @babel/core @babel/preset-env
+yarn add react react-dom
---
+++ client/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from "react";
+
+import AwesomeForm from "./AwesomeForm";
+
+export default class App extends Component {
+  render() {
+    return <AwesomeForm />;
+  }
+}
